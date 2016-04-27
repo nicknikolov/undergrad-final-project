75e28fc6cadfed52c769a53c62199f7c37652148
nicknikolov <nick@nklv.me>
2016-04-27 14:50:45 +0100

    Add Ableton example

--- webinator/server.js
+++ webinator/server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const http = require('http').Server(app)
+
+const osc = require('osc-min')
+const dgram = require('dgram')
+
 app.use(express.static('.'))
 
 const io = require('socket.io')(http)
 
 const remoteIp = '127.0.0.1'
 const remotePort = 6448
+const udpServer = dgram.createSocket('udp4')
 
 const users = {}
 
@@ -29,6 +34,32 @@ io.on('connection', (socket) => {
 
   socket.on('data', (event) => {
     io.to(users[event.id]).emit('inputs', [event.xArray, event.yArray, event.zArray])
+  })
+
+  socket.on('prediction', (event) => {
+    console.log(event)
+    if (event.prediction === 'clip1') {
+      var oscMsg = osc.toBuffer({
+        oscType: 'message',
+        address: '/clip',
+        args: 0
+      })
+
+      console.log('message sent')
+      udpServer.send(oscMsg, 0, oscMsg.length, remotePort, remoteIp)
+    }
+
+    if (event.prediction === 'clip2') {
+      var oscMsg = osc.toBuffer({
+        oscType: 'message',
+        address: '/clip',
+        args: 1
+      })
+
+      console.log('message sent')
+      udpServer.send(oscMsg, 0, oscMsg.length, remotePort, remoteIp)
+    }
+
   })
 })
 
